bf6a4cd4f4d506ce6339a42d358458f767be9bab
Rodwan1 <145416047+Rodwan1@users.noreply.github.com>
2023-11-25 18:38:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,8 +25,8 @@ imageFilenames.forEach((filename, index) => {
   });
 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', 'images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg',)
+newImage.setAttribute('alt', 'Closeup of an eye', 'Swirling rocks', 'Colorful flowers', 'Egyptian art', 'Butterfly');
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
@@ -40,4 +40,4 @@ btn.addEventListener('click', () => {
       btn.textContent = 'Darken';
       overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
-  });
+  }); 
