939c82ef885be348f2b703092e7cd8e72bbaf1d0
Rodwan1 <145416047+Rodwan1@users.noreply.github.com>
2023-11-25 18:17:29 -0500

    handler
    wrote a dark/light button handler, cleaned up code and is ready for publishing

--- main.js
+++ main.js
@@ -24,18 +24,20 @@ imageFilenames.forEach((filename, index) => {
     });
   });
 
-imageFilenames.forEach(filename => {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${filename}`);
-    newImage.setAttribute('alt', `Image ${filename}`);
-    thumbBar.appendChild(newImage);
-
-    
-  });
-
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    if (btn.getAttribute('class') === 'dark') {
+      btn.setAttribute('class', 'light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class', 'dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
