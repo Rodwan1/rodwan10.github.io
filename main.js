143e3ea9bcc4ce9c7f17717c44ea9a4ec44be1fd
Rodwan1 <145416047+Rodwan1@users.noreply.github.com>
2023-11-25 18:50:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFilenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const imageFilenames = ["pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
-const altTexts = ["Closeup of an eye", "Swirling rocks", "Colorful flowers", "Egyptian art", "Butterfly"];
+const altTexts = ["Swirling rocks", "Colorful flowers", "Egyptian art", "Butterfly"];
 
 /* Looping through images */
 
