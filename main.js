86653f0a2fd1c383d303114fd76fb3fed3e7a873
Rodwan1 <145416047+Rodwan1@users.noreply.github.com>
2023-11-25 18:07:54 -0500

    Array. alt names and loops
    declared an array for the images, alternative names for them and made them looping

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const altTexts = ["Closeup of an eye", "Swirling rocks", "Colorful flowers", "Egyptian art", "Butterfly"];
 
 /* Looping through images */
+
+imageFilenames.forEach(filename => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${filename}`);
+    newImage.setAttribute('alt', `Image ${filename}`);
+    thumbBar.appendChild(newImage);
+  });
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
