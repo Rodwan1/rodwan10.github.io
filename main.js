efee9c03f122bb5ebd9c344c6a41c5a899f28c19
Rodwan1 <145416047+Rodwan1@users.noreply.github.com>
2023-11-25 18:14:54 -0500

    click event
    added a click event listener to each image

--- main.js
+++ main.js
@@ -12,11 +12,25 @@ const altTexts = ["Closeup of an eye", "Swirling rocks", "Colorful flowers", "Eg
 
 /* Looping through images */
 
+imageFilenames.forEach((filename, index) => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${filename}`);
+    newImage.setAttribute('alt', altTexts[index]);
+    thumbBar.appendChild(newImage);
+  
+    newImage.addEventListener('click', () => {
+      displayedImage.setAttribute('src', newImage.getAttribute('src'));
+      displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+    });
+  });
+
 imageFilenames.forEach(filename => {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${filename}`);
     newImage.setAttribute('alt', `Image ${filename}`);
     thumbBar.appendChild(newImage);
+
+    
   });
 
 const newImage = document.createElement('img');
